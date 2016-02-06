8867237e5450e9f246f04c9853f57173b5619266
Dana Wheeler <dswheeler777@gmail.com>
2016-02-05 18:34:26 -0600

    XList.txt Management.
    Added GenXList to generate the exclusion list and ReadXList to show the
    contents of XList.txt

--- GenXList.lua
+++ GenXList.lua
@@ -1,4 +1,17 @@
+
+-- Remove prior list if it exists
+trash = shell.run("rm XList.txt")
 -- Open file for writing
--- Read blocks from inventory
--- Write block names to file
--- Close file
+io.output(io.open("XList.txt","w"))
+-- set position to each slot sequentially
+for i = 1,16 do
+  if (turtle.getItemCount(i) > 0) then
+    tmp = turtle.getItemDetail(i)
+    -- Write block name to file
+    io.write(tmp["name"].."\n")
+  end
+end
+-- Write default blocks
+io.write("minecraft:bedrock")
+-- Close the file
+io.close()
--- ReadXList.lua
+++ ReadXList.lua
@@ -1,1 +1,3 @@
--- ReadXList
+file = assert(io.open("XList.txt", "r"))
+for line in file:lines() do print(line) end
+file:close()
